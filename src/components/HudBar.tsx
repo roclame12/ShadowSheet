32f3e6747cc7c78ecfbdabf7735efbd924eb2d59
stephen <roclame12@gmail.com>
2025-10-04 11:53:30 -0400

    Made another internal component <TextStat/> that handles formating stats that would just need a single text segment (for example, the name of the PC and the money that they have)

--- src/components/HudBar.tsx
+++ src/components/HudBar.tsx
@@ -10,7 +10,6 @@ interface StatProps {
     decoration?: ReactElement;  // An extra node that can be used to display extra information about a stat (for example negative modifiers to your stats due to a lack of health)
 }
 
-
 function Stat(props: StatProps) {
     const separator = props.separator !== undefined ? props.separator : "/";
     const decoration = props.decoration ? props.decoration : null;
@@ -25,6 +24,26 @@ function Stat(props: StatProps) {
 }
 
 
+interface TextStatProps {
+    statName: string;           // name of the stat to be displayed
+    statText: string;           // the text of the stat to be displayed
+    decoration?: ReactElement;  // An extra node that can be used to display extra information about a stat (for example negative modifiers to your stats due to a lack of health)
+    className?: string;
+}
+
+function TextStat(props: TextStatProps) {
+    const decoration = props.decoration !== undefined ? props.decoration : null;
+
+    return (
+        <div className="stat-container">
+            <h1 className="stat-header">{props.statName}:</h1>
+            <p className="stat-text">{props.statText}</p>
+            {decoration}
+        </div>
+    )
+}
+
+
 export interface HudBarProps {
 
 }
@@ -33,11 +52,11 @@ export default function HudBar(props: HudBarProps) {
 
     return (
         <div className="hudbar-container">
-            <Stat statName="Name" current={0} max={0}/>
+            <TextStat statName="Name" statText="John Smith"/>
             <Stat statName="Health" current={123} max={123}/>
             <Stat statName="Stun" current={123} max={123}/>
             <Stat statName="Karma" current={123} max={123} separator="|"/>
-            <Stat statName="Money" current={1234} max={5678} separator="" decoration={<p className="stat-text">$</p>}/>
+            <TextStat statName="Money" statText="1234567890" decoration={<p className="stat-text">¥</p>}/>
         </div>
     )
 }
