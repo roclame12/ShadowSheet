aabf3b7e9c4d9246af6252e254e5f863208dbce9
stephen <roclame12@gmail.com>
2025-11-14 20:11:25 -0500

    Added tooltips to the pages in the footer to explain to the user their point

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,18 +13,21 @@ export default function Footer() {
     const [page, setPage] = useState<Pages>(Pages.character);  // state is most likely going to need to be lifted to App.tsx, it's here right now for demo purposes
 
     const pageNames = Object.keys(Pages).filter(key => !isNaN(Number(Pages[key])));
+    const pageTooltips = ["Your character's skills and talents", "Your character's weapons and attacks", "Your character's stuff", "Your character's backstory"]
+
     const elements: ReactElement[] = [];
     for (let i = 0; i < pageNames.length; i++) {
         elements.push(
             <button
                 className="footer-button"
                 onClick={() => { setPage(i) }}
+                title={pageTooltips[i]}
             >
                 { page === i ? <b>{ pageNames[i] }</b> : <p>{ pageNames[i] }</p>}
             </button>
         )
         ;
-        i !== pageNames.length - 1 ? elements.push(<p className="separator"> | </p>) : elements.push();
+        i !== pageNames.length - 1 ? elements.push(<p className="separator">|</p>) : elements.push();
     }
 
     return (
