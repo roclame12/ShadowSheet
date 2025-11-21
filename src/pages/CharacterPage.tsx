ec474c4650ab8380d005e554c4b9296648bd5df2
stephen <roclame12@gmail.com>
2025-11-20 19:07:21 -0500

    Made it so the input boxes that should only accept numbers will only accept them

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -33,11 +33,33 @@ function PersonalData() {
     const [metaType, setMetaType] = useState<string>("human");
     const [ethnicity, setEthnicity] = useState<string>("Italian?");
     const [age, setAge] = useState<string>("21");
-    const [height, setHeight] = useState<string>("6' 0\"");
-    const [weight, setWeight] = useState<string>("123 lbs");
+    const [height, setHeight] = useState<string>("123");
+    const [weight, setWeight] = useState<string>("123");
     const [cred, setCred] = useState<string>("123");
     const [notoriety, setNotoriety] = useState<string>("123");
     const [awareness, setAwareness] = useState<string>("123");
+
+    /**
+     * creates the function that change the update the state of the value associated with an <InputBox/>
+     *
+     * @param setter the React.Dispatch associated with value of the <InputBox/>
+     */
+    function handleInput(setter: React.Dispatch<string>) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => { setter(e.target.value) }
+    }
+
+    /**
+     * Creates a function that will only update the state of the value associated with an <InputBox/> if that input matches
+     * a RegEx pattern.
+     *
+     * @param setter the React.Dispatch associated with the value of the <InputBox/>
+     * @param pattern the regular expression the input must match for the input to update
+     */
+    function handleInputConditional(setter: React.Dispatch<string>, pattern: RegExp) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.target.value === "" || pattern.test(e.target.value)) { setter(e.target.value) }
+        }
+    }
 
     return (
         <div className="personal-data-container" style={{ /*display: "flex", flexDirection: "column", gap: 15 */}}>
@@ -47,56 +69,62 @@ function PersonalData() {
                     header="Metatype"
                     value={metaType}
                     id="meta-type"
-                    onChange={(event) => setMetaType(event.target.value)}
+                    onChange={ handleInput(setMetaType) }
                 />
 
                 <InputBox
                     header="Ethnicity"
                     value={ethnicity}
                     id="ethnicity"
-                    onChange={(event) => setEthnicity(event.target.value)}
+                    onChange={ handleInput(setEthnicity) }
                 />
 
                 <InputBox
                     header="Age"
                     value={age}
+                    maxLen={4}
                     id="age"
-                    onChange={(event) => setAge(event.target.value)}
+                    onChange={ handleInputConditional(setAge, /^\d*$/) }
                 />
 
                 <InputBox
                     header="Height"
                     value={height}
+                    maxLen={4}
                     id="height"
-                    onChange={(event) => setHeight(event.target.value)}
+                    onChange={ handleInputConditional(setHeight, /^\d*$/) }
                 />
 
                 <InputBox
                     header="Weight"
                     value={weight}
+                    maxLen={9}
                     id="weight"
-                    onChange={(event) => setWeight(event.target.value)}
+                    onChange={ handleInputConditional(setWeight, /^\d*$/) }
                 />
 
                 <InputBox
                     header="S. Cred"
                     value={cred}
+                    maxLen={4}
                     id="street-cred"
-                    onChange={(event) => setCred(event.target.value)}
+                    onChange={ handleInputConditional(setCred, /^\d*$/) }
                 />
 
                 <InputBox
                     header="Notoriety"
                     value={notoriety}
+                    maxLen={4}
                     id="notoriety"
-                    onChange={(event) => setNotoriety(event.target.value)}
+                    onChange={ handleInputConditional(setNotoriety, /^\d*$/) }
                 />
 
                 <InputBox
                     header="Awareness"
                     value={awareness}
+                    maxLen={4}
                     id="awareness"
-                    onChange={(event) => setAwareness(event.target.value)}
+                    onChange={ handleInputConditional(setAwareness, /^\d*$/) }
                 />
             </div>
         </div>
